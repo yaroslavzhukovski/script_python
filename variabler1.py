46e854646dac8d6e872003649f2be5d1e32afe14
yaroslavzhukovski <yaroslav.zhukovski@gmail.com>
2024-10-28 17:35:49 +0100

    flytar lektionen

---
+++ variabler1.py
@@ -0,0 +1,52 @@
+# x = 5
+# print(x)
+
+# x = 5
+# y = 3.14
+# namn = 'Bobbo säger "Hej" #/#" 023457'
+# is_student = False
+
+# frukter = ["äpple", "banan", "päron", 8]
+
+# koordinater = (10, 20)
+
+# person = {"namn": "Bobbo", "ålder": 37}
+
+# tal = {1,2,3,4,5,5}
+# print(tal)
+
+# x = 10
+# print(x)
+# x = "Hej"
+# print(x)
+
+# a = 10
+# b = 5
+# summa = a + b
+# print(summa)
+
+# a = 10
+# b = 5
+
+# differens = a - b
+# print(differens)
+
+# a = 10
+# b = 5
+
+# produkt = a * b
+# print(produkt)
+
+# a = 10
+# b = 4
+
+# kvot = a / b
+# print(kvot)
+
+# a = 10
+# b = 4
+
+# kvot = a % b
+# print(kvot)
+
+
